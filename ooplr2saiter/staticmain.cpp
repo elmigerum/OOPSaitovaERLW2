af6c96c6967babe8b9cb4506677ae9dafcd0d5cf
elmirasaitova <saitova-2004@mail.ru>
2024-04-22 16:35:56 +0500

    Oval instead of Triangle

--- ooplr2saiter/staticmain.cpp
+++ ooplr2saiter/staticmain.cpp
@@ -2,11 +2,10 @@
 #include <cstdio>
 
 
-
 class Point {
 public:
-    int x, y;
-public:
+    int x{0}, y{0};
+
     Point() {
         printf("Конструктор по умолчанию создан! Point()\n");
         x = 0;
@@ -19,122 +18,117 @@ public:
         this->y = y;
     }
 
-    Point(Point &p) {
+    Point(Point &p){
         printf("Конструктор копирования создан! Point (&p)\n");
         x = p.x;
         y = p.y;
     }
 
     ~Point() {
-        printf("%d, %d Point() Диструктор сработал!\n ", x, y);
+        printf("%d, %d Point() Деструктор сработал!\n ", x, y);
     }
-    void move(int dx, int dy){
+
+    void move(int dx, int dy) {
         printf("Сработал метод move():\n");
-        x = x+dx;
-        y = y+dy;
+        x = x + dx;
+        y = y + dy;
     }
 };
 
 
+
 //Class ColoredPoint
-class ColoredPoint: public Point {
+class ColoredPoint : public Point {
 public:
-    int color;
-public:
-    ColoredPoint(): Point() {
+    int color{0};
+
+    ColoredPoint() : Point() {
         printf("Конструктор по умлчанию создан! ColoredPoint()\n");
-        color=0;
     }
 
-    ColoredPoint(int x, int y, int color): Point(x,y){
+    ColoredPoint(int x, int y, int color) {
         printf("Конструктор c параметрами создан! ColoredPoint(int x, int y)\n");
-        this->color = color;
+        this -> x = x;
+        this -> y = y;
+        this -> color = color;
     }
 
     ColoredPoint(const ColoredPoint &cp) {
         printf("Конструктор c параметрами создан! ColoredPoint(int x, int y)\n");
-        x = cp.x;
-        y = cp.y;
+        x=cp.x;
+        y=cp.y;
         color = cp.color;
     }
 
     ~ColoredPoint() {
-        printf("Диструктор сработал! (x,y, color) = (%d, %d, %d)\n", x, y, color);
+        printf("Деструктор сработал! (x,y, color) = (%d, %d, %d)\n", x, y, color);
     }
-    void changeColor(int newColor) {
-        color = newColor;
-        printf("Сработал метод changeColor: %d\n", color);
-    }
+
+    void changeColor(int newColor);
+
 };
 
+void ColoredPoint :: changeColor(int newColor) {
+    color = newColor;
+    printf("Сработал метод changeColor: %d\n", color);}
 
-class Triangle {
+class Oval {
 public:
-    Point *A, *B, *C;
+    Point *A;
 public:
-    Triangle(){
-        printf("Создан конструктoр по умолчанию! Triangle() \n");
-        A = new Point(0,0);
-        B = new Point(1,1);
-        C = new Point(1,1);
+    Oval() {
+        printf("Создан конструктoр по умолчанию! Oval() \n");
+        A = new Point(0, 0);
     }
-    Triangle(int x1, int y1, int x2, int y2, int x3, int y3){
-        printf("Создан конструктр c параметрами! Triangle(int x1, int y1, int x2, int y2, int x3, int y3) \n");
-        A = new Point(x1, y1);
-        B = new Point(x2,y2);
-        C = new Point (x3,y3);
+
+    Oval(int x, int y) {
+        printf("Создан конструктр c параметрами! Oval(int x, int y) \n");
+        A = new Point(x, y);
     }
-    Triangle (const Triangle &t) {
-        printf("Создан конструктор копирования! Triangle(const Triangle &t) \n");
+
+    Oval(const Oval &t) {
+        printf("Создан конструктор копирования! Oval(const Oval &t) \n");
         A = new Point(*t.A);
-        B = new Point (*t.B);
-        C = new Point (*t.C);
     }
-    void changePoint(){
-        printf("Координаты треугольника до метода changePoint(): A=(%d, %d), B=(%d, ,%d), C=(%d, %d)\n", A->x, A->y, B->x, B->y, C->x, C->y);
-        int n1 = A -> x;
-        int n2 = A -> y;
-        A ->x = B->x;
-        A ->y = B->y;
-        B ->x = C->x;
-        B ->y = C->y;
-        C ->x = n1;
-        C ->y = n2;
-        printf("Сработал метод changePoint()\n");
-        printf("Координаты треугольника после метода changePoint(): A=(%d, %d), B=(%d, ,%d), C=(%d, %d)\n", A->x, A->y, B->x, B->y, C->x, C->y);
+
+    void moveСenter(int dx, int dy) {
+        printf("Координаты центра овала до метода moveСenter(): A=(%d, %d)\n", A->x, A->y);
+        A->x = A->x+dx;
+        A->y = A->y+dx;
+        printf("Сработал метод moveСenter()\n");
+        printf("Координаты центра овала после метода moveСenter(): A=(%d, %d)\n", A->x, A->y);
     }
-    ~ Triangle(){
-        printf( "%d, %d, %d,%d, %d, %d  Диструктор треугольника сработал!\n", A->x, A->y, B->x, B->y, C->x, C->y);
+
+    ~Oval() {
+        printf("(%d, %d) Деструктор треугольника сработал!\n", A->x, A->y);
         delete A;
-        delete B;
-        delete C;
     }
 };
 
-class Circle{
+class Circle {
 public:
     Point O;
 public:
-    Circle()
-    {
+    Circle() {
         printf("Конструктор по умолчанию создан! Circle()\n");
-        O.x = 0;
-        O.y = 0;
     }
-    Circle(int x1, int y1): O(x1, y1){
+
+    Circle(int x1, int y1) {
         O.x = x1;
         O.y = y1;
         printf("Конструктор с параметрами создан! Circle(int x, int y)\n");
-
     }
-   Circle( Circle &c) {
+
+    Circle(const Circle &c) {
         O = c.O;
         printf("Конструктор копирования создан! Circle(Circle &с)\n");
     }
-    ~ Circle () {
-        printf ("Сработал диструктор круга Circle\n");
+
+    ~Circle() {
+        printf("Сработал деструктор круга Circle\n");
     }
-    void show(){
+
+    void show() {
         printf("Сработал метод show() %d %d \n", O.x, O.y);
     }
 };
@@ -162,16 +156,16 @@ int main() {
 
     /// динамическое создание и удвление
     Point *m = new Point;
-    Point *m2 = new Point(1, 2) ;
-    Point *m3 = new Point ( *m2);
+    Point *m2 = new Point(1, 2);
+    Point *m3 = new Point(*m2);
 
     printf("m = (%d, %d)\n", m->x, m->y);
     printf("m2 = (%d, %d)\n", m2->x, m2->y);
     printf("m3 = (%d, %d)\n", m3->x, m3->y);
 
-    m -> move(12,4);
-    m2->move(2,4);
-    m3->move (3,5);
+    m->move(12, 4);
+    m2->move(2, 4);
+    m3->move(3, 5);
 
     printf("m = (%d, %d)\n", m->x, m->y);
     printf("m2 = (%d, %d)\n", m2->x, m2->y);
@@ -185,7 +179,8 @@ int main() {
 
     /// coloredpoint
     ColoredPoint *cp1 = new ColoredPoint;
-    ColoredPoint *cp3 = new ColoredPoint (1,20,30);
+    cp1->move(2, 1);
+    ColoredPoint *cp3 = new ColoredPoint(1, 20, 30);
     ColoredPoint *cp2 = new ColoredPoint(*cp1);
     printf("Colored point *cp1 = (%d, %d, %d)\n", cp1->x, cp1->y, cp1->color);
     printf("Colored point *cp2 = (%d, %d, %d)\n", cp2->x, cp2->y, cp2->color);
@@ -201,20 +196,20 @@ int main() {
 
     printf("\n\n");
 
-    /// triangle
-    Triangle *t = new Triangle;
-    Triangle *t2 = new Triangle (1,3, 2,3,3,2);
-    Triangle *t1 = new Triangle(*t);
-    t->changePoint();
-    t2->changePoint();
-    t1->changePoint();
+    /// oval
+    Oval *t = new Oval;
+    Oval *t2 = new Oval(1, 3);
+    Oval *t1 = new Oval(*t);
+    t->moveСenter(12,34);
+    t2->moveСenter(1,23);
+    t1->moveСenter(100,2);
     delete t;
     delete t2;
     delete t1;
 
     printf("\n\n");
 
-   /// circle
+    /// circle
     Circle c1;
     printf("Circle c1 = ( %d, %d)\n", c1.O.x, c1.O.y);
     Circle c2(15, 20);
