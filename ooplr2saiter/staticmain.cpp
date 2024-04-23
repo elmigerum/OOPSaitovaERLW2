399698240815ab7b73335d43830823b2d87176ab
elmigerum <167082667+elmigerum@users.noreply.github.com>
2024-04-23 21:00:46 +0500

    Add files via upload
    final version of LW, where initial strings were released and also public methods were replaced to private (respectively, setter and getter functions were added to classes snd main function was rewrote in some fragments).

--- ooplr2saiter/staticmain.cpp
+++ ooplr2saiter/staticmain.cpp
@@ -3,25 +3,21 @@
 
 
 class Point {
-public:
+private:
     int x{0}, y{0};
-
+public:
     Point() {
         printf("Конструктор по умолчанию создан! Point()\n");
-        x = 0;
-        y = 0;
     }
 
-    Point(int x, int y) {
+    Point(int x, int y) : x(x), y(y) {
         printf("Конструктор с параметрами создан! Point(int x, int y)\n");
         this->x = x;
         this->y = y;
     }
 
-    Point(Point &p){
+    Point(const Point &p) : x(p.x), y(p.y) {
         printf("Конструктор копирования создан! Point (&p)\n");
-        x = p.x;
-        y = p.y;
     }
 
     ~Point() {
@@ -33,94 +29,107 @@ public:
         x = x + dx;
         y = y + dy;
     }
-};
 
+    int getX() const {
+        return x;
+    }
+
+    int getY() const {
+        return y;
+    }
+
+    void setX(int newX){
+        x = newX;
+    }
+
+    void setY(int newY){
+        y = newY;
+    }
+
+ };
 
 
 //Class ColoredPoint
 class ColoredPoint : public Point {
-public:
+private:
     int color{0};
-
+public:
     ColoredPoint() : Point() {
-        printf("Конструктор по умлчанию создан! ColoredPoint()\n");
+        printf("Конструктор по умолчанию создан! %d %d %d ColoredPoint()\n", getX(), getY(), color);
     }
 
-    ColoredPoint(int x, int y, int color) {
+    ColoredPoint(int x, int y, int color) : Point(x, y), color(color) {
         printf("Конструктор c параметрами создан! ColoredPoint(int x, int y)\n");
-        this -> x = x;
-        this -> y = y;
-        this -> color = color;
     }
 
-    ColoredPoint(const ColoredPoint &cp) {
-        printf("Конструктор c параметрами создан! ColoredPoint(int x, int y)\n");
-        x=cp.x;
-        y=cp.y;
-        color = cp.color;
+    ColoredPoint(const ColoredPoint &cp) : Point(cp), color(cp.color) {
+        printf("Конструктор копирования создан! ColoredPoint(int x, int y)\n");
     }
 
     ~ColoredPoint() {
-        printf("Деструктор сработал! (x,y, color) = (%d, %d, %d)\n", x, y, color);
+        printf("Деструктор сработал! (x,y, color) = (%d, %d, %d)\n", getX(), getY(), color);
     }
 
     void changeColor(int newColor);
 
+    int getColor(){
+        return color;
+    }
+
+    void setColor(int newColor){
+        color = newColor;
+    }
 };
 
-void ColoredPoint :: changeColor(int newColor) {
-    color = newColor;
-    printf("Сработал метод changeColor: %d\n", color);}
+void ColoredPoint::changeColor(int newColor) {
+    setColor(newColor);
+    printf("Сработал метод changeColor: %d\n", getColor());
+}
+
 
 class Oval {
-public:
+private:
     Point *A;
 public:
-    Oval() {
+    Oval() : A(new Point(0, 0)) {
         printf("Создан конструктoр по умолчанию! Oval() \n");
-        A = new Point(0, 0);
     }
 
-    Oval(int x, int y) {
+    Oval(int x, int y) : A(new Point(x, y)) {
         printf("Создан конструктр c параметрами! Oval(int x, int y) \n");
-        A = new Point(x, y);
     }
 
-    Oval(const Oval &t) {
+    Oval(const Oval &t) : A(new Point(*(t.A))) {
         printf("Создан конструктор копирования! Oval(const Oval &t) \n");
-        A = new Point(*t.A);
     }
 
-    void moveСenter(int dx, int dy) {
-        printf("Координаты центра овала до метода moveСenter(): A=(%d, %d)\n", A->x, A->y);
-        A->x = A->x+dx;
-        A->y = A->y+dx;
-        printf("Сработал метод moveСenter()\n");
-        printf("Координаты центра овала после метода moveСenter(): A=(%d, %d)\n", A->x, A->y);
+    void moveCenter(int dx, int dy) {
+        printf("Координаты центра овала до метода moveСenter(): A=(%d, %d)\n", A->getX(), A->getY());
+        A->setX(A->getX() + dx);
+        A->setY(A->getY() + dy);
+        printf("Сработал метод moveСenter(%d, %d)\n", dx, dy);
+        printf("Координаты центра овала после метода moveСenter(): A=(%d, %d)\n", A->getX(), A->getY());
     }
-
     ~Oval() {
-        printf("(%d, %d) Деструктор треугольника сработал!\n", A->x, A->y);
+        printf("(%d, %d) Деструктор овала сработал!\n", A->getX(), A->getY());
         delete A;
     }
+
 };
 
 class Circle {
-public:
+private:
     Point O;
 public:
     Circle() {
         printf("Конструктор по умолчанию создан! Circle()\n");
     }
 
-    Circle(int x1, int y1) {
-        O.x = x1;
-        O.y = y1;
+    Circle(int x1, int y1) : O(x1, y1) {
         printf("Конструктор с параметрами создан! Circle(int x, int y)\n");
     }
 
-    Circle(const Circle &c) {
-        O = c.O;
+    Circle(const Circle &c) : O(c.O) {
         printf("Конструктор копирования создан! Circle(Circle &с)\n");
     }
 
@@ -129,7 +138,7 @@ public:
     }
 
     void show() {
-        printf("Сработал метод show() %d %d \n", O.x, O.y);
+        printf("Сработал метод show() %d %d \n", O.getX(), O.getY());
     }
 };
 
@@ -141,15 +150,15 @@ int main() {
         Point m;
         Point m2(1, 2);
         Point m3(m2);
-        printf("m = (%d, %d)\n", m.x, m.y);
-        printf("m2 = (%d, %d)\n", m2.x, m2.y);
-        printf("m3 = (%d, %d)\n", m3.x, m3.y);
+        printf("m = (%d, %d)\n", m.getX(), m.getY());
+        printf("m2 = (%d, %d)\n", m2.getX(), m2.getY());
+        printf("m3 = (%d, %d)\n", m3.getX(), m3.getY());
         m.move(12, 45);
         m2.move(23, 45);
         m3.move(2, 34);
-        printf("m = (%d, %d)\n", m.x, m.y);
-        printf("m2 = (%d, %d)\n", m2.x, m2.y);
-        printf("m3 = (%d, %d)\n", m3.x, m3.y);
+        printf("m = (%d, %d)\n", m.getX(), m.getY());
+        printf("m2 = (%d, %d)\n", m2.getX(), m2.getY());
+        printf("m3 = (%d, %d)\n", m3.getX(), m3.getY());
     }
 
     printf("\n\n");
@@ -159,17 +168,17 @@ int main() {
     Point *m2 = new Point(1, 2);
     Point *m3 = new Point(*m2);
 
-    printf("m = (%d, %d)\n", m->x, m->y);
-    printf("m2 = (%d, %d)\n", m2->x, m2->y);
-    printf("m3 = (%d, %d)\n", m3->x, m3->y);
+    printf("m = (%d, %d)\n", m->getX(), m->getY());
+    printf("m2 = (%d, %d)\n", m2->getX(), m2->getY());
+    printf("m3 = (%d, %d)\n", m3->getX(), m3->getY());
 
     m->move(12, 4);
     m2->move(2, 4);
     m3->move(3, 5);
 
-    printf("m = (%d, %d)\n", m->x, m->y);
-    printf("m2 = (%d, %d)\n", m2->x, m2->y);
-    printf("m3 = (%d, %d)\n", m3->x, m3->y);
+    printf("m = (%d, %d)\n", m->getX(), m->getY());
+    printf("m2 = (%d, %d)\n", m2->getX(), m2->getY());
+    printf("m3 = (%d, %d)\n", m3->getX(), m3->getY());
 
     delete m;
     delete m2;
@@ -179,12 +188,11 @@ int main() {
 
     /// coloredpoint
     ColoredPoint *cp1 = new ColoredPoint;
-    cp1->move(2, 1);
     ColoredPoint *cp3 = new ColoredPoint(1, 20, 30);
     ColoredPoint *cp2 = new ColoredPoint(*cp1);
-    printf("Colored point *cp1 = (%d, %d, %d)\n", cp1->x, cp1->y, cp1->color);
-    printf("Colored point *cp2 = (%d, %d, %d)\n", cp2->x, cp2->y, cp2->color);
-    printf("Colored point *cp = (%d, %d, %d)\n", cp3->x, cp3->y, cp3->color);
+    printf("Colored point *cp1 = (%d, %d, %d)\n", cp1->getX(), cp1->getY(), cp1->getColor());
+    printf("Colored point *cp2 = (%d, %d, %d)\n", cp2->getX(), cp2->getY(), cp2->getColor());
+    printf("Colored point *cp = (%d, %d, %d)\n", cp3->getX(), cp3->getY(), cp3->getColor());
 
     cp1->changeColor(12);
     cp2->changeColor(1);
@@ -200,9 +208,9 @@ int main() {
     Oval *t = new Oval;
     Oval *t2 = new Oval(1, 3);
     Oval *t1 = new Oval(*t);
-    t->moveСenter(12,34);
-    t2->moveСenter(1,23);
-    t1->moveСenter(100,2);
+    t->moveCenter(12, 34);
+    t2->moveCenter(1, 23);
+    t1->moveCenter(100, 2);
     delete t;
     delete t2;
     delete t1;
@@ -211,11 +219,8 @@ int main() {
 
     /// circle
     Circle c1;
-    printf("Circle c1 = ( %d, %d)\n", c1.O.x, c1.O.y);
     Circle c2(15, 20);
-    printf("Circle c2 = ( %d, %d)\n", c2.O.x, c2.O.y);
     Circle c3(c2);
-    printf("Circle c3 = ( %d, %d)\n", c3.O.x, c3.O.y);
     c1.show();
     c2.show();
     c3.show();
